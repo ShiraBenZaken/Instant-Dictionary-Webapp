17d8db604a4b9563c82f25f5f513f7cb0e94cc39
SHIRA BEN ZAKEN <shira441192@gmail.com>
2021-08-05 22:05:55 +0300

    create main and start routing

---
+++ webapp/home.py
@@ -0,0 +1,19 @@
+import justpy as jp
+
+
+class Home:
+    path = "/home"
+
+    def serve(self):
+        wp = jp.QuasarPage(tailwind=True)
+        div = jp.Div(a=wp, classes="bg-grey-200 h-screen")
+        jp.Div(a=div, text="This is Home Page", classes="text-4xl m-2")
+        jp.Div(a=div, text="""
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        In rutrum erat faucibus tellus tincidunt, at tristique arcu elementum.
+         Morbi tempor sagittis suscipit.
+          Nunc dictum aliquet purus, sit amet tincidunt dui consectetur placerat. 
+        """, classes="text-lg")
+        return wp
+
+
